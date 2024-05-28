796bff095156d8a43f10dd2dfd4d0f2fdb43168c
rajgarg021 <rajgarg021@gmail.com>
2024-05-28 14:48:36 +0530

    defined a single encoder layer of CLIP

--- models/clip.py
+++ models/clip.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from attention import SelfAttention
 
 
-class CLIPEmbedding(nn.Module):
+class CLIPEmbeddings(nn.Module):
     """
     Module to generate embeddings for CLIP
     """
@@ -19,8 +19,46 @@ class CLIPEmbedding(nn.Module):
 
     def forward(self, tokens):
         
-        # (B, seq_len) -> (B, seq_len, Dim)
-        x = self.token_embeddings(tokens)
-        x += self.position_embedddings
+        # (B, seq_len) -> (B, seq_len, d_embed)
+        embeddings = self.token_embeddings(tokens)
+        embeddings += self.position_embedddings
+
+        return embeddings
+
+
+class CLIPLayer(nn.Module):
+    """
+    Single encoder layer of CLIP
+    """
+
+    def __init__(self, n_heads: int, d_embed: int):
+        super().__init__()
+
+        # self.n_head = n_head
+        # self.head_size = d_embed // n_head
+
+        self.norm1 = nn.LayerNorm(d_embed)
+        self.attention = SelfAttention(n_heads, d_embed)
+        self.norm2 = nn.LayerNorm(d_embed)
+        self.ln1 = nn.Linear(d_embed, 4 * d_embed)
+        self.ln2 = nn.Linear(4 * d_embed, d_embed)
+
+    def forward(self, x: torch.tensor):
+        """ x: (B, seq_len, d_embed) """
+
+        # SELF ATTENTION
+        residue = x
+        x = self.norm1(x)
+        x = self.attention(x, causal_mask=True)
+        x += residue
+
+        # FEED FORWARD
+        residue = x
+        x = self.norm2(x)
+        x = self.ln1(x)
+        x = x * torch.sigmoid(1.702 * x) # quick GELU activation function
+        x = self.ln2(x)
+        x += residue
 
         return x
+    
