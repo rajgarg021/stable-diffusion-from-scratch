9f931773d595605b185d131fe0b13fa6fe9ec603
rajgarg021 <rajgarg021@gmail.com>
2024-06-03 18:45:10 +0530

    defined the Unet (neural network for the reverse diffusion process)

--- models/diffusion.py
+++ models/diffusion.py
@@ -23,9 +23,151 @@ class TimeEmbeddings(nn.Module):
         return x
         
 
-class UNet():
+class ResidualBlock():
     def __init__(self):
         raise NotImplementedError
+    
+
+class AttentionBlock():
+    def __init__(self):
+        raise NotImplementedError
+    
+
+class Upsample(nn.Module):
+    def __init__(self):
+        raise NotImplementedError
+    
+
+class SwitchSequential(nn.Sequential):
+    """
+    Applying parameters based on the layer
+    """
+
+    def forward(self, x, context, time):
+        for layer in self:
+            if isinstance(layer, AttentionBlock):
+                x = layer(x, context)
+            elif isinstance(layer, ResidualBlock):
+                x = layer(x, time)
+            else:
+                x = layer(x)
+        return x
+
+
+class Unet(nn.Module):
+    """
+    neural network for the reverse diffusion process
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.encoders = nn.ModuleList([
+            # (B, 4, H/8, W/8) -> (B, 320, H/8, W/8)
+            SwitchSequential(nn.Conv2d(4, 320, kernel_size=3, padding=1)),
+            
+            # (B, 320, H/8, W/8) -> (B, 320, H/8, W/8) -> (B, 320, H/8, W/8)
+            SwitchSequential(ResidualBlock(320, 320), AttentionBlock(8, 40)),
+            
+            # (B, 320, H/8, W/8) -> (B, 320, H/8, W/8) -> (B, 320, H/8, W/8)
+            SwitchSequential(ResidualBlock(320, 320), AttentionBlock(8, 40)),
+            
+            # (B, 320, H/8, W/8) -> (B, 320, H/16, W/16)
+            SwitchSequential(nn.Conv2d(320, 320, kernel_size=3, stride=2, padding=1)),
+            
+            # (B, 320, H/16, W/16) -> (B, 640, H/16, W/16) -> (B, 640, H/16, W/16)
+            SwitchSequential(ResidualBlock(320, 640), AttentionBlock(8, 80)),
+            
+            # (B, 640, H/16, W/16) -> (B, 640, H/16, W/16) -> (B, 640, H/16, W/16)
+            SwitchSequential(ResidualBlock(640, 640), AttentionBlock(8, 80)),
+            
+            # (B, 640, H/16, W/16) -> (B, 640, H/32, W/32)
+            SwitchSequential(nn.Conv2d(640, 640, kernel_size=3, stride=2, padding=1)),
+            
+            # (B, 640, H/32, W/32) -> (B, 1280, H/32, W/32) -> (B, 1280, H/32, W/32)
+            SwitchSequential(ResidualBlock(640, 1280), AttentionBlock(8, 160)),
+            
+            # (B, 1280, H/32, W/32) -> (B, 1280, H/32, W/32) -> (B, 1280, H/32, W/32)
+            SwitchSequential(ResidualBlock(1280, 1280), AttentionBlock(8, 160)),
+            
+            # (B, 1280, H/32, W/32) -> (B, 1280, H/64, W/64)
+            SwitchSequential(nn.Conv2d(1280, 1280, kernel_size=3, stride=2, padding=1)),
+            
+            # (B, 1280, H/64, W/64) -> (B, 1280, H/64, W/64)
+            SwitchSequential(ResidualBlock(1280, 1280)),
+            
+            # (B, 1280, H/64, W/64) -> (B, 1280, H/64, W/64)
+            SwitchSequential(ResidualBlock(1280, 1280)),
+        ])
+
+        self.bottleneck = SwitchSequential(
+            # (B, 1280, H/64, W/64) -> (B, 1280, H/64, W/64)
+            ResidualBlock(1280, 1280), 
+            
+            # (B, 1280, H/64, W/64) -> (B, 1280, H/64, W/64)
+            AttentionBlock(8, 160), 
+            
+            # (B, 1280, H/64, W/64) -> (B, 1280, H/64, W/64)
+            ResidualBlock(1280, 1280), 
+        )
+        
+        self.decoders = nn.ModuleList([
+            # (B, 2560, H/64, W/64) -> (B, 1280, H/64, W/64)
+            SwitchSequential(ResidualBlock(2560, 1280)),
+            
+            # (B, 2560, H/64, W/64) -> (B, 1280, H/64, W/64)
+            SwitchSequential(ResidualBlock(2560, 1280)),
+            
+            # (B, 2560, H/64, W/64) -> (B, 1280, H/64, W/64) -> (B, 1280, H/32, W/32) 
+            SwitchSequential(ResidualBlock(2560, 1280), Upsample(1280)),
+            
+            # (B, 2560, H/32, W/32) -> (B, 1280, H/32, W/32) -> (B, 1280, H/32, W/32)
+            SwitchSequential(ResidualBlock(2560, 1280), AttentionBlock(8, 160)),
+            
+            # (B, 2560, H/32, W/32) -> (B, 1280, H/32, W/32) -> (B, 1280, H/32, W/32)
+            SwitchSequential(ResidualBlock(2560, 1280), AttentionBlock(8, 160)),
+            
+            # (B, 1920, H/32, W/32) -> (B, 1280, H/32, W/32) -> (B, 1280, H/32, W/32) -> (B, 1280, H/16, W/16)
+            SwitchSequential(ResidualBlock(1920, 1280), AttentionBlock(8, 160), Upsample(1280)),
+            
+            # (B, 1920, H/16, W/16) -> (B, 640, H/16, W/16) -> (B, 640, H/16, W/16)
+            SwitchSequential(ResidualBlock(1920, 640), AttentionBlock(8, 80)),
+            
+            # (B, 1280, H/16, W/16) -> (B, 640, H/16, W/16) -> (B, 640, H/16, W/16)
+            SwitchSequential(ResidualBlock(1280, 640), AttentionBlock(8, 80)),
+            
+            # (B, 960, H/16, W/16) -> (B, 640, H/16, W/16) -> (B, 640, H/16, W/16) -> (B, 640, H/8, W/8)
+            SwitchSequential(ResidualBlock(960, 640), AttentionBlock(8, 80), Upsample(640)),
+            
+            # (B, 960, H/8, W/8) -> (B, 320, H/8, W/8) -> (B, 320, H/8, W/8)
+            SwitchSequential(ResidualBlock(960, 320), AttentionBlock(8, 40)),
+            
+            # (B, 640, H/8, W/8) -> (B, 320, H/8, W/8) -> (B, 320, H/8, W/8)
+            SwitchSequential(ResidualBlock(640, 320), AttentionBlock(8, 40)),
+            
+            # (B, 640, H/8, W/8) -> (B, 320, H/8, W/8) -> (B, 320, H/8, W/8)
+            SwitchSequential(ResidualBlock(640, 320), AttentionBlock(8, 40)),
+        ])
+
+    def forward(self, x, context, time):
+        """
+        x: (B, 4, H/8, W/8)
+        context: (B, seq_len, d_embed) 
+        time: (1, 1280)
+        """
+
+        skip_connections = []
+        for layers in self.encoders:
+            x = layers(x, context, time)
+            skip_connections.append(x)
+
+        x = self.bottleneck(x, context, time)
+
+        for layers in self.decoders:
+            # since we always concat with the skip connection of the encoder, the number of features increases before being sent to the decoder's layer
+            x = torch.cat((x, skip_connections.pop()), dim=1) 
+            x = layers(x, context, time)
+        
+        return x
 
 
 class UNetFinalLayer():
